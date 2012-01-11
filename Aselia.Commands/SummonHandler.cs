24fb9aa9ad508e601a5aacd25af9680c25c5795b
Zenexer <techsoftadvanced@gmail.com>
2012-01-11 17:34:56 -0500

    SUMMON command gives disabled notice.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/SummonHandler.cs
@@ -0,0 +1,16 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(SummonHandler.CMD, Authorizations.Normal)]
+	public sealed class SummonHandler : ICommand
+	{
+		public const string CMD = "SUMMON";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.SendNumeric(Numerics.ERR_SUMMONDISABLED, ":Summoning is disabled on this network.");
+		}
+	}
+}
