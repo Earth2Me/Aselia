371f18354b6bc2259ad7df3e47bb61e0872f7a65
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 11:53:19 -0500

    Added placeholders for GHOST and REGAIN commands.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/GhostHandler.cs
@@ -0,0 +1,17 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(GhostHandler.CMD, Authorizations.Registered)]
+	public sealed class GhostHandler : ICommand
+	{
+		public const string CMD = "GHOST";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			// TODO: Implement command
+			e.User.SendNumeric(Numerics.ERR_UNKNOWNERROR, ":That command is not yet implemented.");
+		}
+	}
+}
---
+++ Aselia.Commands/RegainHandler.cs
@@ -0,0 +1,17 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(RegainHandler.CMD, Authorizations.Registered)]
+	public sealed class RegainHandler : ICommand
+	{
+		public const string CMD = "REGAIN";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			// TODO: Implement command
+			e.User.SendNumeric(Numerics.ERR_UNKNOWNERROR, ":That command is not yet implemented.");
+		}
+	}
+}
