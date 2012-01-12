45b2e6ff6b8174c24a340b6b6011f0589d8350bc
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 15:04:34 -0500

    Added LUSERS command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/LusersHandler.cs
@@ -0,0 +1,16 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(LusersHandler.CMD, Authorizations.Unregistered)]
+	public sealed class LusersHandler : ICommand
+	{
+		public const string CMD = "LUSERS";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.ReplyMotd();
+		}
+	}
+}
