ca7b4a1c87fcebe692891f637951b34fbbcc426f
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 12:31:03 -0500

    Added VERSION command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/VersionHandler.cs
@@ -0,0 +1,16 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(VersionHandler.CMD, Authorizations.Unregistered)]
+	public sealed class VersionHandler : ICommand
+	{
+		public const string CMD = "VERSION";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.ReplyVersion();
+		}
+	}
+}
