0f57de87a5247f3effca5a1699e12a5831e9c291
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 15:05:30 -0500

    Added USERS command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/UsersHandler.cs
@@ -0,0 +1,16 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(UsersHandler.CMD, Authorizations.Unregistered)]
+	public sealed class UsersHandler : ICommand
+	{
+		public const string CMD = "USERS";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.ReplyMotd();
+		}
+	}
+}
