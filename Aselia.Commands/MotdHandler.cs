1889937cf460ca27f65a9b8b04a12f4e41d5bcd9
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 15:05:01 -0500

    Added MOTD command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/MotdHandler.cs
@@ -0,0 +1,16 @@
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(MotdHandler.CMD, Authorizations.Unregistered)]
+	public sealed class MotdHandler : ICommand
+	{
+		public const string CMD = "MOTD";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.ReplyMotd();
+		}
+	}
+}
