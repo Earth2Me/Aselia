fbcfac6fa50971ce28266e95405b8a10932cf5c1
Zenexer <techsoftadvanced@gmail.com>
2012-01-12 12:21:29 -0500

    Added COMMIT command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/CommitHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Aselia.Common;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(CommitHandler.CMD, Authorizations.NetworkOperator)]
+	public sealed class CommitHandler : ICommand
+	{
+		public const string CMD = "COMMIT";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			e.User.SendNumeric(Numerics.RPL_COMMITSTART, ":Committing cache...");
+			try
+			{
+				e.Server.CommitCache();
+				e.User.SendNumeric(Numerics.RPL_COMMITSUCCESS, ":The cache was successfully committed.");
+			}
+			catch (Exception ex)
+			{
+				e.User.SendNumeric(Numerics.ERR_COMMITFAIL, ":The commit failed:", ex.Message);
+			}
+		}
+	}
+}
