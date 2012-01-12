fb8ddad48b2767699aaae635a2fda8dae38938b3
Zenexer <techsoftadvanced@gmail.com>
2012-01-11 19:14:48 -0500

    Added KILL command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/KillHandler.cs
@@ -0,0 +1,33 @@
+﻿using Aselia.Common;
+using Aselia.Common.Core;
+using Aselia.Common.Modules;
+
+namespace Aselia.UserCommands
+{
+	[Command(KillHandler.CMD, Authorizations.NetworkOperator, "{1} {0} :{2}")]
+	public sealed class KillHandler : ICommand
+	{
+		public const string CMD = "KILL";
+
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			if (e.Arguments.Length < 1)
+			{
+				e.User.ErrorNeedMoreParams(CMD);
+				return;
+			}
+
+			UserBase user = e.Server.GetUser(e.Arguments[0]);
+			if (user == null)
+			{
+				e.User.SendNumeric(Numerics.ERR_NOSUCHNICK, e.Arguments[0], ":That nickname does not exist.");
+				return;
+			}
+
+			string reason = e.Arguments.Length > 1 ? "Killed: " + e.Arguments[1] : "Killed by network operator.";
+
+			e.User.BroadcastExclusive("QUIT", e.User.Mask, reason);
+			e.User.SendCommand(CMD, e.Server.Id, reason);
+		}
+	}
+}
