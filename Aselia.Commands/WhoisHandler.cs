afd7e80897fd693b969f9cb4677b3bee9217de4d
Zenexer <techsoftadvanced@gmail.com>
2012-01-15 08:44:02 -0500

    Implemented WHOIS command.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

--- Aselia.Commands/WhoisHandler.cs
+++ Aselia.Commands/WhoisHandler.cs
@@ -1,4 +1,6 @@
-﻿using Aselia.Common;
+﻿using System.Text;
+using Aselia.Common;
+using Aselia.Common.Core;
 using Aselia.Common.Modules;
 
 namespace Aselia.UserCommands
@@ -10,8 +12,73 @@ namespace Aselia.UserCommands
 
 		public void Handler(object sender, ReceivedCommandEventArgs e)
 		{
-			// TODO: Implement command
-			e.User.SendNumeric(Numerics.ERR_UNKNOWNERROR, ":That command is not yet implemented.");
+			if (e.Arguments.Length < 1)
+			{
+				e.User.ErrorNeedMoreParams(CMD);
+				return;
+			}
+
+			UserBase user = e.Server.GetUser(e.Arguments[0]);
+			if (user == null)
+			{
+				e.User.SendNumeric(Numerics.ERR_NOSUCHNICK, e.Arguments[0], ":That user is not online.");
+				return;
+			}
+
+			e.User.SendNumeric(Numerics.RPL_WHOISUSER, user.Mask.Nickname, user.Mask.Username, user.Mask.Hostname, "*", ":" + user.Gecos);
+
+			// TODO: RPL_WHOISSERVER
+
+			switch (e.User.Level)
+			{
+			case Authorizations.NetworkOperator:
+				e.User.SendNumeric(Numerics.RPL_WHOISOPERATOR, user.Mask.Nickname, ":is a network operator.");
+				break;
+
+			case Authorizations.Service:
+				e.User.SendNumeric(Numerics.RPL_WHOISOPERATOR, user.Mask.Nickname, ":is a network service.");
+				break;
+			}
+
+			StringBuilder channels = new StringBuilder();
+			foreach (ChannelBase c in user.Channels.Values)
+			{
+				if (e.User.Level < Authorizations.NetworkOperator && c.HasFlag("Secret") && !e.User.IsVoice(c) && !e.User.Channels.ContainsKey(c.Id))
+				{
+					continue;
+				}
+
+				if (channels.Length > 0)
+				{
+					channels.Append(' ');
+				}
+
+				string prefix = c.GetPrefix(user);
+				if (!string.IsNullOrEmpty(prefix))
+				{
+					if (e.User.HasSessionFlag("MultiPrefix"))
+					{
+						channels.Append(prefix);
+					}
+					else
+					{
+						channels.Append(prefix[0]);
+					}
+				}
+
+				channels.Append(c.Name);
+			}
+			if (channels.Length > 0)
+			{
+				e.User.SendNumeric(Numerics.RPL_WHOISCHANNELS, user.Mask.Nickname, ":" + channels.ToString());
+			}
+
+			if (user.Level >= Authorizations.Registered)
+			{
+				e.User.SendNumeric(Numerics.RPL_WHOISACCOUNT, user.Mask.Nickname, user.Mask.Account, ":is logged in as");
+			}
+
+			e.User.SendNumeric(Numerics.RPL_ENDOFWHOIS, user.Mask.Nickname, ":End of user information.");
 		}
 	}
 }
