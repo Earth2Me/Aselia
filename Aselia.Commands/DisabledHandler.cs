b42b6c1fae4c330f95f8a0b1e89ad605bfcfacd4
Zenexer <techsoftadvanced@gmail.com>
2012-01-15 08:01:10 -0500

    Moved SUMMON to list of permanently disabled commands.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

--- Aselia.Commands/DisabledHandler.cs
+++ Aselia.Commands/DisabledHandler.cs
@@ -1,12 +1,13 @@
-﻿
+﻿using Aselia.Common;
+using Aselia.Common.Modules;
+
 namespace Aselia.UserCommands
 {
-	[Command("WHO WHOWAS", Authorizations.Unregistered)]
+	[Command("SUMMON WHO WHOWAS", Authorizations.Unregistered)]
 	public sealed class DisabledHandler : ICommand
 	{
 		public void Handler(object sender, ReceivedCommandEventArgs e)
 		{
-			// TODO: Implement command
 			e.User.SendNumeric(Numerics.ERR_COMMANDDISABLED, e.Command, ":That command is disabled on this network.");
 		}
 	}
