6e95debf33a77ea24f06266e9a4137ec88553938
Zenexer <techsoftadvanced@gmail.com>
2012-01-15 07:43:15 -0500

    Marked WHO and WHOWAS as permanently disabled.
    Signed-off-by: Zenexer <techsoftadvanced@gmail.com>

---
+++ Aselia.Commands/DisabledHandler.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Aselia.UserCommands
+{
+	[Command("WHO WHOWAS", Authorizations.Unregistered)]
+	public sealed class DisabledHandler : ICommand
+	{
+		public void Handler(object sender, ReceivedCommandEventArgs e)
+		{
+			// TODO: Implement command
+			e.User.SendNumeric(Numerics.ERR_COMMANDDISABLED, e.Command, ":That command is disabled on this network.");
+		}
+	}
+}
